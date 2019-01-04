27b3c97d0f9bf8e1eda2c0086cb200f9acf88212
Tim Joosten <Tjoosten3@gmail.com>
2019-01-04 07:08:54 +0100

    Update

---
+++ resources/views/helpdesk/dashboards/huurder.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="page-header">
+        <h1 class="page-title">Helpdesk</h1>
+        
+        <div class="page-subtitle">Stel hier je vraag omtrent het domein of verhuur</div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-3">
+        </div>
+
+        <div class="col-9">
+            <div class="col-12"> {{-- Information content --}}
+                <div class="alert alert-info shadow-sm mb-3">
+                    <p>
+                        Om u een goede opvolging of assistentie aan te bieden wij u een helpdesk in het leven geroepen waarop u al uw vragen kunt stellen. 
+                        Naar aanloop van je verhuring of wanneer je aanwezig bent in onze lokalen. Zodat wij jouw snel al je problemen, opmerken of vragen kunnen beantwoorden.                
+                    </p>
+
+                    <p class="font-weight-bold mb-0">
+                        <i class="fe fe-alert-circle mr-1"></i> 
+                        Voor dringende zaken kunt u best de verantwoordelijke aanspreken die jouw verhuur behandeld.
+                    </p>
+                </div>
+            </div> {{-- /// EINDE informatie content --}}
+        </div>
+    </div>
+@endsection
