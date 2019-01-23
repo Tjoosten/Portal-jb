d7860999a404e27b8f9a0676941ede2c476000b8
Tjoosten <Topairy@gmail.com>
2019-01-23 15:42:53 +0100

    Work on lease validator

--- app/Http/Controllers/Lease/Tenants/BillableController.php
+++ app/Http/Controllers/Lease/Tenants/BillableController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Lease\Tenants;
 
+use App\Http\Requests\Tenants\BillableValidator;
 use App\User;
-use Illuminate\Http\{Request, Response};
+use Illuminate\Http\{Request, Response, RedirectResponse};
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 
@@ -38,15 +39,34 @@ class BillableController extends Controller
             return view('tenants.billable-overview', $viewData);
         }
 
-        // 
+        // De gegeven gebruikers entiteit is geen huurder. Dus is de facturatie data niet nodig.
+        // Aangezien administrators en leiding de verhuur onderling kunnen regelen met de verantwoordelijke of VZW.
         abort(Response::HTTP_NOT_FOUND);
     }
 
     /**
+     * Methode voor het aanpassen van de facturatie data van de huurder.
      *
+     * @param  BillableValidator $input The form validation class dat verantwoordelijk is voor de validatie.
+     * @param  User              $user  De databank entiteit van de huurder in de applicatie.
+     * @return RedirectResponse
      */
-    public function store(User $user): RedirectResponse
+    public function store(BillableValidator $input, User $user): RedirectResponse
     {
-        return redirect()->route('tenants.show', $information->user);
+        // Check of de gegeven gebruiker een huurder is. En dat alle facturatie data is aangepast door
+        // de ->billingInformation(); relatie.
+        if ($user->hasRole('huurder') && $information = $user->billingInformation()->update($input->all())) {
+
+            // Indien de gegeven gebruiker niet de zelfde is dan de aangemelde gebruiker.
+            // Moet deze actie gelogd worden in het systeem.
+            if ($this->auth->user()->is($user)) {
+                $user->billingInformation()->logActivity("Heeft de facturatie gegevens voor {$user->name} aangepast", 'Facturatie');
+            }
+
+            return redirect()->route('tenants.show', $information->user);
+        }
+
+        // Kan de facturatie niet aanpassen dus herleid de aangemelde gebruiker terug naar de facturatie gegevens weergave.
+        return redirect()->route('tenants.billing', $user);
     }
 }
